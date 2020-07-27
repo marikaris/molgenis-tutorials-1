0029be612b615b6e9adfd0b3915d6c6e3679e508
mkslofstra <mariska-karina@hotmail.com>
2020-07-27 10:42:16 +0200

    feat(home): show description of tutorials on hover

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,7 +20,8 @@
             </div>
             <div class="col-md-2 col-sm-3 col-xs-4">
               <github-button href="https://github.com/molgenis/molgenis" data-size="large" data-show-count="true"
-                             aria-label="Star molgenis/molgenis on GitHub">Star</github-button>
+                             aria-label="Star molgenis/molgenis on GitHub">Star
+              </github-button>
             </div>
           </div>
           <div class="row">
@@ -33,25 +34,42 @@
             <div class="col-md-10">
               <div class="thumbnails">
                 <h3>Tutorials</h3>
-                <div class="box-card" v-for="(tutorial, index) in tutorials" :key="index">
-                  <router-link :to="tutorial.infos.path" @click.native="click">
-                    <div class="embedded-slideshow-container">
-                      <component :is="tutorial" :embedded=true :keyboardNavigation="false"
-                                 :mouseNavigation="false"></component>
+                <div class="row" @mouseenter="setHover" @mouseleave="setHover">
+                  <div v-for="(tutorial, index) in tutorials" :key="index"
+                       :class="isStacked ? 'col-md-4' : 'col-md-12'"
+                       class="mg-tutorial">
+                    <div class="box-card" :class="isStacked ? 'col-md-12' : 'col-md-3'">
+                      <router-link :to="tutorial.infos.path" @click.native="click">
+                        <div class="embedded-slideshow-container">
+                          <component :is="tutorial" :embedded=true :keyboardNavigation="false"
+                                     :mouseNavigation="false"/>
+                        </div>
+                      </router-link>
+                      <div class="mg-caption">
+                        <p class="font-weight-bold">{{tutorial.infos.title}}</p>
+                        <p class="thumbnail-description d-md-none d-lg-none d-xl-none">
+                          {{tutorial.infos.description}}
+                        </p>
+                      </div>
                     </div>
-                  </router-link>
-                  <div class="mg-caption">
-                    <h3>{{tutorial.infos.title}}</h3>
-                    <p class="thumbnail-description">
-                      {{tutorial.infos.description}}
-                    </p>
+                    <transition name="fade">
+                      <div class="mg-description d-none d-md-block d-xl-block col-md-9" v-if="isVisible">
+                        <h4>{{tutorial.infos.title}}</h4>
+                        <p>
+                          {{tutorial.infos.description}}
+                        </p>
+                      </div>
+                    </transition>
                   </div>
                 </div>
+
               </div>
             </div>
             <div class="col-md-2">
               <h3>Resources</h3>
-              <a :href="`${publicPath}data/demo.xlsx`" download><font-awesome-icon icon="download" /> Demo EMX</a>
+              <a :href="`${publicPath}data/demo.xlsx`" download>
+                <font-awesome-icon icon="download"/>
+                Demo EMX</a>
             </div>
           </div>
         </div>
@@ -73,7 +91,9 @@ export default {
   data: function () {
     return {
       tutorials: tutorials.list,
-      publicPath: process.env.BASE_URL
+      publicPath: process.env.BASE_URL,
+      isVisible: false,
+      isStacked: true
     }
   },
   mounted: function () {
@@ -82,14 +102,34 @@ export default {
   methods: {
     click (evt) {
       evt.stopPropagation()
+    },
+    setStacked () {
+      this.isStacked = !this.isStacked
+    },
+    setHover () {
+      this.isVisible = !this.isVisible
+      this.isStacked ? this.setStacked() : setTimeout(this.setStacked, 400)
     }
   }
 }
 </script>
 <style lang='scss' scoped>
+  .mg-description {
+    background-color: rgba(53, 53, 53, 0.9);
+    color: white;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+
   .mg-tutorials-header {
     margin-top: 0;
   }
+
   .banner {
     padding-left: 12%;
     padding-right: 12%;
@@ -99,12 +139,13 @@ export default {
 
   #mg-top-banner {
     font-family: 'IBM Plex Mono', monospace;
-    background-color:rgb(1, 127, 253);
+    background-color: rgb(1, 127, 253);
     color: white;
     font-size: 1.6rem;
     line-height: 3.0rem;
   }
-  #mg-top-banner h1{
+
+  #mg-top-banner h1 {
     border-top: 0.5rem solid;
     font-size: 4.8rem;
     line-height: 4.8rem;
