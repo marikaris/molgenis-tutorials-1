2a032bc2102c6a40409a068732847dbeb466e031
mkslofstra <mariska-karina@hotmail.com>
2020-07-31 09:54:52 +0200

    test(molgenistutorialpreview): fix tests (WIP)
    Linter is still angry because of Wrapper<any>

--- tests/unit/components/MolgenisTutorialPreview.spec.ts
+++ tests/unit/components/MolgenisTutorialPreview.spec.ts
@@ -1,12 +1,8 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, Wrapper } from '@vue/test-utils'
 import MolgenisTutorialPreview from '@/components/MolgenisTutorialPreview.vue'
 
-function sleep (ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
-
 describe('MolgenisTutorialPreview', () => {
-  let wrapper: any
+  let wrapper: Wrapper<any>
 
   beforeEach(() => {
     wrapper = shallowMount(MolgenisTutorialPreview, { propsData: { tutorials: [] }, stubs: ['router-link'] })
@@ -16,19 +12,33 @@ describe('MolgenisTutorialPreview', () => {
     expect(wrapper).toBeDefined()
   })
 
-  describe('setHover', () => {
-    it('should set isVisible to true and isStacked to false', () => {
-      wrapper.vm.setHover()
+  describe('atMouseEnter', () => {
+    it('should set inFocus to true and change isVisible to true and isStacked to false after 500 ms', () => {
+      wrapper.vm.atMouseEnter()
+      expect(wrapper.vm.inFocus).toBe(true)
+      wrapper.vm.showDescription()
       expect(wrapper.vm.isVisible).toBe(true)
       expect(wrapper.vm.isStacked).toBe(false)
     })
-    it('should set isVisible to false and isStacked to true after 500 ms', () => {
+  })
+
+  describe('atMouseLeave', () => {
+    beforeEach(() => {
+      // set state that equals state when element is in focus
       wrapper.vm.isVisible = true
       wrapper.vm.isStacked = false
-      wrapper.vm.setHover()
+      jest.useFakeTimers()
+    })
+
+    it('should set inFocus to false and change isVisible to false and isStacked to true after 1000 ms', () => {
+      wrapper.vm.atMouseLeave()
+      expect(wrapper.vm.inFocus).toBe(false)
+      wrapper.vm.hideDescription()
+      expect(setTimeout).toHaveBeenCalledTimes(1)
+      expect(setTimeout).toHaveBeenCalledWith(wrapper.vm.setStackedTrue, 500)
+      jest.runAllTimers()
       expect(wrapper.vm.isVisible).toBe(false)
-      expect(wrapper.vm.isStacked).toBe(false)
-      sleep(500).then(() => { expect(wrapper.vm.isStacked).toBe(true) })
+      expect(wrapper.vm.isStacked).toBe(true)
     })
   })
 })
